ff0a2086a1854812dced50cf47704ea42ce7e766
Darpan Shah <darpanshah08@gmail.com>
2017-07-20 14:52:56 -0400

    Converted maven repo.
    - Added dummy files.

---
+++ src/main/java/com/apt464/project/Apt464.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Darpan Shah Jul 20, 2017
+ */ 
+package com.apt464.project;
+
+public class Apt464 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/com/apt464/project/Apt464Test.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Darpan Shah Jul 20, 2017
+ */ 
+package com.apt464.project;
+
+/**
+ * @author DARPAN
+ *
+ */
+public class Apt464Test {
+
+}
